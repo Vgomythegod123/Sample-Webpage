2b5d2e7adee7826feef5388476cafe57bdc4a81b
Vgomythegod123 <114121256+Vgomythegod123@users.noreply.github.com>
2022-09-21 21:15:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function name(){
+    document.getElementById("name").style.color="white";
+}
+
+function image(){
+    document.getElementById("image").src="https://sneakernews.com/wp-content/uploads/2022/07/air-jordan-12-concord-ao6068-004-5.jpg";
+}
